72d7ad81c86fc7de52a9ed000de6d93ed7f2d753
felix2300 <45297327+felix2300@users.noreply.github.com>
2021-06-29 15:39:25 +0200

    start
    Start files

---
+++ src/main/java/de/vendettagroup/real_emeralds/Real_emeralds.java
@@ -0,0 +1,17 @@
+package de.vendettagroup.real_emeralds;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Real_emeralds extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
